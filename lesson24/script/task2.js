6de363bea1f56e1212f1a111382270d64c3ed535
Dmytro-Len <msalmanac@gmail.com>
2021-02-01 08:15:26 +0200

    t pu

---
+++ lesson24/script/task2.js
@@ -0,0 +1,2 @@
+let a = Number(prompt('Enter first number'))
+let b = Number(prompt('Enter second number'))
---
+++ lesson24/script/task6.js
@@ -0,0 +1,8 @@
+let a = +prompt("Введите первое число")
+let b = +prompt("Введите второе число")
+let c = prompt("Введите знак")
+let result;
+
+while (confirm) {
+    
+}
