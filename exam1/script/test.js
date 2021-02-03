96b024ef5d877a2465e3055b34e82246e1944311
Dmytro-Len <msalmanac@gmail.com>
2021-02-03 19:04:35 +0200

    nothing

---
+++ exam1/script/test.js
@@ -0,0 +1,6 @@
+function main(){
+    // document.getElementById('choose__title').innerHTML='Because we are the best'
+    // document.getElementById('choose__title').style.color='#5474fd'
+    document.getElementById('nav').style.display='none'
+}
+
