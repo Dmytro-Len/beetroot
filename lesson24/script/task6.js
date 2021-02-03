57106b2c08ff7dca0e4a98814f8806ed11482d60
Dmytro-Len <msalmanac@gmai.com>
2021-02-03 15:22:01 +0200

    finished circles

--- lesson24/script/task6.js
+++ lesson24/script/task6.js
@@ -1,8 +1,9 @@
-let a = +prompt("Введите первое число")
-let b = +prompt("Введите второе число")
-let c = prompt("Введите знак")
-let result;
-
-while (confirm) {
-    
+while (true) {
+    const enterDigits = prompt('Enter 2 digits and operator for resolving using comas for example: 2, 2, + . Your turn: ');
+    const digitArr = enterDigits.split(',');
+    alert(eval(digitArr[0] + digitArr[2] + digitArr[1]))
+    let answer = prompt('Would you like to calculate more? (yes, no)')
+    if (answer === 'no') {
+        break;
+    }
 }
---
+++ lesson24/script/task9.js
@@ -0,0 +1,5 @@
+for (let num = 2; num <= 9; num++) {
+    for (let multiplier = 1; multiplier <= 10; multiplier++) {
+        console.log(num.toString() + "x" + multiplier.toString() + "=" + num * multiplier);
+    }
+}
