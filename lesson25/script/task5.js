617bc1d27957dbe389788a1c484ac1271c18b922
Dmytro-Len <msalmanac@gmail.com>
2021-02-05 00:27:57 +0200

    m

--- lesson25/script/task5.js
+++ lesson25/script/task5.js
@@ -1,10 +1,17 @@
-// let enter = +prompt('Enter your number')
-
-// function perfect(){
-//     while (enter % )
-
-// }
-
-// perfect(enter)
-
-
+num = +prompt('ВВедите число')
+function perfectNumber(num) {
+    let sum = 0;
+    let reminder;
+    for (let i = 1; i < num - 1; i++) {
+        reminder = num % 1;
+        if (reminder === 0) {
+            sum += 1;
+        }
+    }
+    if (num === sum) {
+        alert(num + 'Совершенное число')
+    } else {
+        alert('Не совершенное число')
+    }
+}
+perfectNumber()
