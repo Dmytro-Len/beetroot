fb552005012c2cdf3e7dee48dbae9c49e6853ce9
Dmytro-Len <msalmanac@gmail.com>
2021-02-09 23:15:16 +0200

    finished

--- lesson27/script/task1.js
+++ lesson27/script/task1.js
@@ -5,12 +5,37 @@ const shoppingList = [
     {name: 'bread', count: 1, status: 'not bought'}
 ]
 
-//купленные продукты 
-// let boughtProducts = shoppingList.every(shoppingList => console.log(shoppingList.status === 'bought'))
+// купленные продукты
+
 function boughtProducts() {
-    let i = 0;
-    for(i; i < shoppingList.length; i++) {
-        
-    }
+
+   console.log(shoppingList.sort(function(a, b) {
+
+       if (a.status > b.status) {
+           return 1
+       } if ((a.status < b.status)) {
+           return -1;
+       } return 0
+       
+   }))
 }
 boughtProducts()
+
+// добавление покупки в список
+function addProduct(a) {
+    console.log(a.name)
+    let i =0
+    for (i; i < shoppingList.length; i++) {
+        (a.name !== (shoppingList[i].name || shoppingList[i].name || shoppingList[i].name || shoppingList[i].name)) ? console.log('alert') : shoppingList[i].count++;
+    }
+    console.log(shoppingList)
+}
+addProduct(product = {name: 'bread'})
+
+
+//покупка продукта
+function buyProducts(a) {
+    a = 'bought'
+    console.log(a)
+}
+buyProducts(shoppingList[1].status)
