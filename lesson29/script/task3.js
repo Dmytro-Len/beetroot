2d05aeca57e2dab339116e061c4c18ff491366b8
Dmytro-Len <msalmanac@gmail.com>
2021-02-15 18:06:13 +0200

    29lesson

--- lesson29/script/task3.js
+++ lesson29/script/task3.js
@@ -1,10 +1,17 @@
+let count = -1;
 let goAhead = () => {
-    let count = 0;
-    if (count === 1) {
-            document.querySelector('.top').style.backgroundColor = 'blue';
-            document.querySelector('.middle').style.backgroundColor = 'blue';
-            document.querySelector('.bottom ').style.backgroundColor = 'blue';
-        } if (count === 2) {
+    count += 1;
+    if (count === 0) {
+            document.querySelector('.top').style.backgroundColor = 'red';
+            document.querySelector('.middle').style.backgroundColor = 'white';
+            document.querySelector('.bottom ').style.backgroundColor = 'white';
+    } if (count === 1) {
+            document.querySelector('.top').style.backgroundColor = 'white';
             document.querySelector('.middle').style.backgroundColor = 'yellow';
-        }
+            document.querySelector('.bottom ').style.backgroundColor = 'white';
+    } if (count === 2) {
+        document.querySelector('.top').style.backgroundColor = 'white';
+        document.querySelector('.middle').style.backgroundColor = 'white';
+        document.querySelector('.bottom ').style.backgroundColor = 'green';
     }
+}
