cb73d7c1fa934b902dbf299060bfac1286c506ef
Dmytro-Len <msalmanac@gmail.com>
2021-02-15 10:08:05 +0200

    lesson29

---
+++ lesson29/script/task2.js
@@ -0,0 +1,7 @@
+let closeModal = () => {
+    document.querySelector('#modal').style.display = 'none';
+}
+let showModal = () => {
+    // alert('hi, Dima')
+    document.querySelector('#modal').style.display = 'block';
+}
---
+++ lesson29/script/task3.js
@@ -0,0 +1,10 @@
+let goAhead = () => {
+    let count = 0;
+    if (count === 1) {
+            document.querySelector('.top').style.backgroundColor = 'blue';
+            document.querySelector('.middle').style.backgroundColor = 'blue';
+            document.querySelector('.bottom ').style.backgroundColor = 'blue';
+        } if (count === 2) {
+            document.querySelector('.middle').style.backgroundColor = 'yellow';
+        }
+    }
