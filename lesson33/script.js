9e3709179e709084ff9ab85889eb97d4aca9c571
Dmytro-Len <msalmanac@gmail.com>
2021-02-24 22:19:27 +0200

    1 part of ajax

--- lesson33/script.js
+++ lesson33/script.js
@@ -1,6 +1,13 @@
-fetch('https://jsonplaceholder.typicode.com/users/')
+const loadButton = document.querySelector('.user__btn')
+
+loadButton.addEventListener('click', () =>{
+  fetch('https://jsonplaceholder.typicode.com/users/')
   .then(response => response.json())
-  .then(json => {
-      console.log(json[2].name)
-      
+  .then(data => {
+    const tag = document.querySelector('.user')
+
+    for (i = 0; i < data.length; i++) {
+      tag.insertAdjacentHTML('afterbegin', `<p>User: ${data[i].name}, Tel: ${data[i].phone}, ADDRESS: ${data[i].address.city}, ${data[i].address.street}</p>`);
+    }
   })
+})
