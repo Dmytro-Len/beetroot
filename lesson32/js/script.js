ee4584fb2f26c194cd22dbc178403c58551e2a1b
Dmytro-Len <msalmanac@gmail.com>
2021-02-25 00:54:20 +0200

    slick slider

---
+++ lesson32/js/script.js
@@ -0,0 +1,10 @@
+$('.slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    centerMode: true,
+    variableWidth: true,
+    prevArrow: $('.left-tick'),
+    nextArrow: $('.right-tick')
+  });
