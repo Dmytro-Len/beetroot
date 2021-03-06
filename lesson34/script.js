f1657d697395140e5ead2b86797ff1bdb380b8d8
Dmytro-Len <msalmanac@gmail.com>
2021-03-06 12:33:35 +0200

    empty

--- lesson34/script.js
+++ lesson34/script.js
@@ -1,27 +1,39 @@
-// function initMap() {
-//     const container = document.querySelector('#map')
-//     const map = new google.maps.Map(container, {
-//         center: {lat: 50.514859621333, lng: 30.411714860733756},
-//         zoom: 4,
-//     })
-// }
-
-
 function initMap() {
      const map = new google.maps.Map(document.getElementById("map"), {
       center: { lat: 50.46551807542966, lng: 30.354173434658293 },
       zoom: 15,
       icon: 'maps-and-flags.svg',
-
     });
+const position = { lat: 50.55, lng: 28.656664386571386 };
+const destination = { lat: 60.2586905455587, lng: 28.686664386571386 };
+const dirServ = new google.maps.DirectionsService()
+const dirRend = new google.maps.DirectionsRenderer()
+
+const routeOptions = {
+  travelMode: 'DRIVING',
+  origin: position,
+  destination: destination,
+}
+
+dirRend.setMap(map)
+
+dirServ.route(routeOptions, (result, status)=> {
+  if (status === 'OK') {
+    dirRend.setDirections(result)
+  } else {
+    alert('Wrong route!')
   }
+  // console.log(result)
+  // console.log(status)
+})
 
-//   const marker = new google.maps.Marker({
-//       position,
-//       map,
-//       icon: './maps-and-flags.svg',
-//       draggable: 'true',
-      
-//   })
+}
 
-///ROUTE homework
+  document.querySelector('#submit-btn').addEventListener('click', function() {
+    // console.log('it works')
+  let changeBg = document.createElement('h3')
+  changeBg.className = 'afterclick'
+  changeBg.innerHTML = 'Route has been created';
+  changeBg.style.textAlign = 'center'
+  document.body.append(changeBg);
+  });
