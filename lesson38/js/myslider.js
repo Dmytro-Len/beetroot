a47cb11ec8f810fa6de4ac575a4da4af1ff78a0d
Dmytro-Len <msalmanac@gmail.com>
2021-03-15 08:23:06 +0200

    message

---
+++ lesson38/js/myslider.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.slider').slick({
+      Infinite: true,
+      dots: false,
+      arrows: false,
+      autoplay: true,
+      autoplaySpeed: 3000,
+    });
+  });
+      
